d857cab70906b0b6638df02d43a45318fbf64ba6
Katsuyoshi Ito <kito@itosoft.com>
2023-08-15 16:44:49 +0900

    implement RD and RDS commands.
    - refactoring constructors of LadderDriveDevice.

--- IRBoardLib/IRBoard.cs
+++ IRBoardLib/IRBoard.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 
 public class IRBoard
@@ -33,27 +34,120 @@ public class IRBoard
         Console.WriteLine("Connected with a new client");
 
         // クライアント接続ごとに処理を非同期で実行
-        _ = HandleClientAsync(client);
+        _ = HandleClientAsync(this, client);
+    }
+  }
+
+
+  private object lockObj = "lockObj";
+  private Dictionary<LadderDriveDevice, UInt16> deviceDict = new Dictionary<LadderDriveDevice, UInt16>();
+
+  private UInt16 GetValueOfDevice(LadderDriveDevice device) {
+    UInt16 val = 0;
+    lock(lockObj) {
+      if (deviceDict.ContainsKey(device)) {
+        val = deviceDict[device];
+      }
+    }
+    return val;
+  }
+
+  private void SetValueToDevice(UInt16 value, LadderDriveDevice device) {
+    lock(lockObj) {
+      deviceDict[device] = value;
+    }
+  }
+
+  private string RdResponse(string[] elements) {
+    if (elements.Length != 2) { return "E1"; }
+    var a = elements[1].Split(".");
+    var d = new LadderDriveDevice(a[0]);
+    var format = a.Length >= 2 && a[1].ToUpper() == "H" ? "X" : "";
+    return GetValueOfDevice(d).ToString(format);
+  }
+  
+  private string RdsResponse(string[] elements) {
+    if (elements.Length != 3) { return "E1"; }
+
+    try { 
+      var a = elements[1].Split(".");
+      var d = new LadderDriveDevice(a[0]);
+      var c = int.Parse(elements[2]);
+      string[] res = new string[c];
+      var format = a.Length >= 2 && a[1].ToUpper() == "H" ? "X" : "";
+
+      for (int i = 0; i < c; i++) {
+        res[i] = GetValueOfDevice(d).ToString(format);
+        d = d.NextDevice;
+      }
+      return string.Join(" ", res);
+    } catch {
+      return "E1";
     }
   }
   
-  private static async Task HandleClientAsync(TcpClient client)
+  private string StResponse(string[] elements) {
+    if (elements.Length > 2) { return "E1"; }
+    return "OK";
+  }
+  
+  private string RsResponse(string[] elements) {
+    if (elements.Length > 2) { return "E1"; }
+    return "OK";
+  }
+  
+  private string WrResponse(string[] elements) {
+    if (elements.Length > 2) { return "E1"; }
+    return "OK";
+  }
+  
+  private string WrsResponse(string[] elements) {
+    if (elements.Length > 2) { return "E1"; }
+    return "OK";
+  }
+  
+  private static async Task HandleClientAsync(IRBoard board, TcpClient client)
   {
       try
       {
-          // クライアントからのデータを受信、送信する処理をここに記述
-          // 例えば、NetworkStreamを使用してデータの送受信を行う
           using (NetworkStream stream = client.GetStream())
           {
-              byte[] buffer = new byte[1024];
-              int bytesRead;
-
-              while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-              {
-                  // データを受信し、必要な処理を行う
-                  // ここでは単に受信したデータをそのままクライアントに返す例を示します
-                  await stream.WriteAsync(buffer, 0, bytesRead);
+            using (StreamReader reader = new StreamReader(stream)) {
+              using (StreamWriter writer = new StreamWriter(stream)) {
+                while(true) {
+                  string message = reader.ReadLine();
+                  if (message != null) {
+                    var elements = message.Split(' ');
+                    var response = "";
+                    switch (elements[0].ToUpper()) {
+                      case "RD":
+                        response = board.RdResponse(elements);
+                        break;
+                      case "RDS":
+                        response = board.RdsResponse(elements);
+                        break;
+                      case "ST":
+                        response = board.StResponse(elements);
+                        break;
+                      case "RS":
+                        response = board.RsResponse(elements);
+                        break;
+                      case "WR":
+                        response = board.WrResponse(elements);
+                        break;
+                      case "WRS":
+                        response = board.WrsResponse(elements);
+                        break;
+                      default:
+                        response = "E1";
+                        break;
+                    }
+                    writer.WriteLine(response);
+                    writer.Flush();
+                  }
+                }
               }
+            }
           }
       }
       catch (Exception ex)
--- IRBoardLib/LadderDriveDevice.cs
+++ IRBoardLib/LadderDriveDevice.cs
@@ -3,6 +3,12 @@ using System.Text.RegularExpressions;
 
 public class LadderDriveDevice {
 
+  public LadderDriveDevice(string prefix, int number) {
+    Prefix = prefix;
+    Number = number;
+    CheckAvailable();
+  }
+
   public LadderDriveDevice(string name) {
     var pattern = "^(X|Y|M|D|L|H|SC|SD|CC|CS|C|TC|TS|T)([0-9a-f]+)$";
     var matches = Regex.Matches(name, pattern, RegexOptions.IgnoreCase);
@@ -14,10 +20,8 @@ public class LadderDriveDevice {
         case "Y":
           try {
             Number = int.Parse(match.Groups[2].Value, System.Globalization.NumberStyles.HexNumber);
-            if (Number < 1024) {
-              IsAvailable = true;
-            }
-          } catch {}
+            CheckAvailable();
+          } catch { return; }
           break;
         case "C":
         case "CC":
@@ -27,27 +31,63 @@ public class LadderDriveDevice {
         case "TS":
           try {
             Number = int.Parse(match.Groups[2].Value);
-            if (Number < 256) {
-              IsAvailable = true;
-            }
-          } catch {}
+            CheckAvailable();
+          } catch { return; }
           break;
         default:
           try {
             Number = int.Parse(match.Groups[2].Value);
-            if (Number < 1024) {
-              IsAvailable = true;
-            }
-          } catch {}
+            CheckAvailable();
+          } catch { return; }
           break;
       }
     }
   }
+
+  private void CheckAvailable() {
+    switch (Prefix) {
+      case "X":
+      case "Y":
+        if (Number < 1024) {
+          IsAvailable = true;
+        }
+        break;
+      case "C":
+      case "CC":
+      case "CS":
+      case "T":
+      case "TC":
+      case "TS":
+        if (Number < 256) {
+          IsAvailable = true;
+        }
+        break;
+      default:
+        if (Number < 1024) {
+          IsAvailable = true;
+        }
+        break;
+    }
+  }
+
   public string Prefix { get; private set; } = "";
 
   public int Number { get; private set; } = 0;
 
   public bool IsAvailable { get; private set; } = false;
+
+  public bool IsBitDevice {
+    get {
+      switch (Prefix) {
+        case "X":
+        case "Y":
+        case "M":
+          return true;
+        default:
+          return false;
+      }
+    }
+  }
 
   public string Name {
     get {
@@ -60,5 +100,26 @@ public class LadderDriveDevice {
       }
     }
   }
+
+  public override int GetHashCode() {
+    return Name.GetHashCode();
+  }
+
+  public override bool Equals(object obj) {
+    return obj is LadderDriveDevice device && Name == device.Name;
+  }
+
+  public LadderDriveDevice PrevDevice {
+    get {
+      return new LadderDriveDevice(Prefix, Number - 1);
+    }
+  }
+
+  public LadderDriveDevice NextDevice {
+    get {
+      return new LadderDriveDevice(Prefix, Number + 1);
+    }
+  }
+
 
 }
